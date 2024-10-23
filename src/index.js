ce73ee2537b5fdfa5da91ad9bbfb3560e243a715
CaptainKirioto <mrg.zhi@gmail.com>
2024-10-23 22:29:11 +0200

    stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-const message = "Hello, Jack";
+import express from 'express';
 
-console.log(message);
+import contacts from './db/contacts.js';
+
+const app = express();
+
+// Exampe 1
+
+// app.get('/', (request, response) => {
+//   response.send('<h1>Home Page</h1>');
+// });
+
+// app.get('/contacts', (request, response) => {
+//   console.log(request.method);
+//   console.log(request.url);
+//   response.send('<h1>Contacts Page</h1>');
+// });
+
+// app.listen(3000, () =>
+//   console.log('Web-server successfully running on 3000 port'),
+// );
+
+// Example 2
+
+app.get('/contacts', (_, res) => {
+  res.send(contacts);
+});
+
+app.listen(3000, () =>
+  console.log('Web-server successfully running on 3000 port'),
+);
