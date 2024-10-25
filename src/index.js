9e336059ae7ee167ea7e7b8d0acc39a4938027ff
CaptainKirioto <mrg.zhi@gmail.com>
2024-10-25 16:05:07 +0200

    hungry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-
 import contacts from './db/contacts.js';
+import cors from 'cors';
+import pino from 'pino-http';
 
 const app = express();
 
@@ -22,8 +23,65 @@ const app = express();
 
 // Example 2
 
-app.get('/contacts', (_, res) => {
-  res.send(contacts);
+// app.set('json spaces', 4);
+// app.get('/contacts', (_, res) => {
+//   res.json(contacts); // it's better then res.send while it can return null and it includes app.set settings
+//   // res.send(contacts);
+// });
+
+// app.listen(3000, () =>
+//   console.log('Web-server successfully running on 3000 port'),
+// );
+
+// Example 3
+
+app.use(cors());
+const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+  },
+});
+// app.use(logger);
+
+// const corsMiddleware = cors();
+// app.use(corsMiddleware);
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-Requested-With,content-type',
+//   );
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('First middleware');
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('Second Middleware');
+//   next();
+// });
+
+app.get('/products', (req, res) => {
+  res.json([]);
+});
+
+app.get('/contacts', (req, res) => {
+  res.json(contacts);
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: `${req.url} not found`,
+  });
 });
 
 app.listen(3000, () =>
