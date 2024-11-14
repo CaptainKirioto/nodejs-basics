925777e8b3ff2dd58247529762180f126ce6a9ac
CaptainKirioto <mrg.zhi@gmail.com>
2024-11-14 20:00:33 +0100

    auth started

--- project-backend/src/server.js
+++ project-backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { env } from './utils/env.js';
 import contactsRouter from './routers/contacts.js';
+import authRouter from './routers/auth.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { logger } from './middlewares/logger.js';
@@ -19,6 +20,7 @@ export const startServer = () => {
   app.use(express.json());
 
   // Якщо прийде будь-який запит, що починається з /contacts, шукай обробник цього запиту у об'єкті contactsRouter
+  app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
 
   app.use(notFoundHandler); // when address is not found, should be placed right after routes
